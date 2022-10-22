f16c8e71a0ff9c6fd7deec04798db64445b9a144
Paulina D <pdevito1188@gmail.com>
2022-10-22 13:40:07 -0400

    Initial Commit

---
+++ CamCount/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CamCount
+//
+//  Created by Paulina DeVito on 10/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
